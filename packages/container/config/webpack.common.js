383c6c7c71f7464a876eb8b5231fd12044d22b26
Balint Csikos <balint.csikos.priv@gmail.com>
2022-05-19 20:43:08 +0200

    first commit

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,22 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react',  // transform react jsx tags
+                            '@babel/preset-env'     // transform in diff ways es16...
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime'   // enable async await syntax
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
---
+++ packages/container/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+export default () => {
+    return <div><h1>Hi there!</h1><hr /><MarketingApp /></div>
+}
